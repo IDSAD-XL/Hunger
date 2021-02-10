2016bc8fa3908f13a763c156d4a5a943eaa8f5f6
IDSAD <idsad.ch@gmail.com>
2021-02-11 01:44:05 +0200

    Slider block / no mobile adapatation

--- Hunger/js/common.js
+++ Hunger/js/common.js
@@ -1,6 +1,10 @@
 $('.block__spec_slider').slick({
+	arrows: false,
 	dots: true,
   	infinite: true,
   	speed: 300,
-  	slidesToShow: 1
+  	slidesToShow: 1,
+	autoplay: true,
+	autoplaySpeed: 10000,
+	cssEase: 'cubic-bezier(0.87, 0, 0.13, 1)'
 })
